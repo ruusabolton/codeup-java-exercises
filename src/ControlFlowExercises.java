0ce58256ed94af5ab072c2e82823229ef494dfd8
Ruusa Bolton <ruusabolton@ruusas-mbp.home>
2018-07-24 22:02:53 -0500

    adding refactored for-loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,27 +2,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-//        boolean condition = true;
-//  Create a do-while loop that starts at 2,
-// and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
-//        2
-//        4
-//        16
-//        256
-//        65536
-        //
-        int i = 2;
-        int powerOfTwo = i ;
+//      refactor the previous two exercises to use a for loop instead
+//
+//        Alter your loop to count backwards by 5's from 100 to -10.
 
 
-          do {
-                  System.out.println(powerOfTwo);   // print out the power of two
-                  powerOfTwo = powerOfTwo * powerOfTwo;  // double to get the next one
+        for (int i = 100; i >= -10; i-=5){
+            System.out.println(i);
 
-              // repeat until i equals n
-
-              i++;
-         } while (i<=1000000);
+        }
     }
 }
 
