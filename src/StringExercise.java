1447a3a976255164cc4d5e47210bb3bcce4e21e4
Ruusa Bolton <ruusabolton@Ruusas-MacBook-Pro.local>
2018-07-25 13:51:12 -0500

    adding exercise 1 - paragraph

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 public class StringExercise {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+// 1.   For each of the following output examples,
+//    create a string variable named message that contains the desired output and print it out to the console.
 
+String message = "We don't need no education\n" +
+        "We don't need no thought control\n\n"+
+        "Check \"this\" out!, \"s inside of \"s!\n\n"+
+               "In windows, the main drive is usually C:\\ \n\n"+
+       "I can do backslashes \\, double backslashes \\\\, \n"+
+        "and the amazing triple backslash \\\\\\! \n";
 
-
-
+System.out.print(message);
 
     }
 }
